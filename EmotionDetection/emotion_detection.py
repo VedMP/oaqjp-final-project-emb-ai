9e70b074585414af26f325f2112871e63b9c55ef
VedMP <vedmpatel2005@gmail.com>
2025-08-13 14:46:54 -0400

    Added error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,8 +1,8 @@
 """
 This Module is responsible for understanding the emotion of the text the user provide
 """
-import requests
 import json
+import requests
 # Function that given 'text_to_analyze' gets the emotions using Watson NLP
 # libaray's emotion Predict function via an API call.
 def emotion_detector(text_to_analyze):
@@ -14,19 +14,23 @@ def emotion_detector(text_to_analyze):
     headers =  {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     my_object = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url,json = my_object, headers=headers)
-    emotionPredictions = json.loads(response.text) #Turn the response text into a json file.
-
+    #If the status code is a client error then return the dict with all values set to None
+    if response.status_code == 400:
+        return_dict =  {'anger': None, 'disgust': None, 'fear': None,'joy': None,'sadness': None}
+        return_dict['dominant_emotion'] = None
+        return return_dict
+    
+    emotion_Predictions = json.loads(response.text) #Turn the response text into a json file.
     #Get the scores of the emotions using
     # emotionPredictions['emotionPredictions'][0]['emotion']['emotion_name'].
-    anger_score = emotionPredictions['emotionPredictions'][0]['emotion']['anger'] 
-    disgust_score = emotionPredictions['emotionPredictions'][0]['emotion']['disgust']
-    fear_score = emotionPredictions['emotionPredictions'][0]['emotion']['fear']
-    joy_score = emotionPredictions['emotionPredictions'][0]['emotion']['joy']
-    sadness_score = emotionPredictions['emotionPredictions'][0]['emotion']['sadness']
+    anger_score = emotion_Predictions['emotionPredictions'][0]['emotion']['anger'] 
+    disgust_score = emotion_Predictions['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = emotion_Predictions['emotionPredictions'][0]['emotion']['fear']
+    joy_score = emotion_Predictions['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = emotion_Predictions['emotionPredictions'][0]['emotion']['sadness']
     dominant_emotion = ""
-    high_score =0
+    high_score = 0
     return_dict =  {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score,'joy': joy_score,'sadness': sadness_score}
-
     # Finds dominant emotion using comparison to find the emotion with the highest 
     # score and set it dominant emotion in 'return_dict'
     for emotion in return_dict.keys():
